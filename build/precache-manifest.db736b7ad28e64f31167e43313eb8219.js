5162b5e090d6862595fdf63f7b4566610627c470
Gerwin Jonathan <gerwinjh12345@gmail.com>
2019-06-27 14:58:30 +0700

    Test Simple Login with ReactJS

---
+++ build/precache-manifest.db736b7ad28e64f31167e43313eb8219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98adb21b823e0d24885d64713c3481fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b89a6865b38b0b54e1f",
+    "url": "/static/css/main.a7d3a96c.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "/static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "7b89a6865b38b0b54e1f",
+    "url": "/static/js/main.b19d2917.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/React.47ce6e77.png"
+  },
+  {
+    "revision": "2ad58311f98382d9dec4ad0284bd4e17",
+    "url": "/static/media/lock.2ad58311.png"
+  }
+]);
